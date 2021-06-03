51c83e6a02ea0c29caba6723c1d14cce18ef7b3e
Ricardo <ricardo.apu.chinchilla@cr.ey.com>
2021-06-03 00:29:41 -0600

    feat: change background color with state

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View, ViewStyle } from "react-native";
 
 export default function App() {
+  var randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
+  function invertHex(hex: string) {
+    let inverted = (Number(`0x1${hex}`) ^ 0xffffff)
+      .toString(16)
+      .substr(1)
+      .toUpperCase();
+    while (inverted.length < 5) {
+      inverted += "0";
+    }
+    return inverted;
+  }
+  const [color, setColor] = useState(randomColor());
+  const containerStyle = {
+    backgroundColor: `#${color}`,
+  } as ViewStyle;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={[styles.container, containerStyle]}>
+      <Text style={{ color: `#${invertHex(color)}`, fontWeight: "bold" }}>
+        Color: #{color}
+      </Text>
+      <Button
+        title="Change color"
+        color={`#${color}`}
+        onPress={() => {
+          setColor(randomColor());
+        }}
+      ></Button>
     </View>
   );
 }
@@ -12,8 +37,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
