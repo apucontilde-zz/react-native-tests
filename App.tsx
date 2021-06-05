ead93aa7419e91d61cfce35ade2ae8dcb85696dd
Ricardo <ricardo.apu.chinchilla@cr.ey.com>
2021-06-04 23:44:20 -0600

    todo

--- App.tsx
+++ App.tsx
@@ -1,35 +1,14 @@
 import React, { useState } from "react";
 import { Button, StyleSheet, Text, View, ViewStyle } from "react-native";
+import Todo from "./components/Todo";
 
+const todos = ["todo","todo 2","todo 3"];
 export default function App() {
-  var randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
-  function invertHex(hex: string) {
-    let inverted = (Number(`0x1${hex}`) ^ 0xffffff)
-      .toString(16)
-      .substr(1)
-      .toUpperCase();
-    while (inverted.length < 5) {
-      inverted += "0";
-    }
-    return inverted;
-  }
-  const [color, setColor] = useState(randomColor());
-  const containerStyle = {
-    backgroundColor: `#${color}`,
-  } as ViewStyle;
-
   return (
-    <View style={[styles.container, containerStyle]}>
-      <Text style={{ color: `#${invertHex(color)}`, fontWeight: "bold" }}>
-        Color: #{color}
-      </Text>
-      <Button
-        title="Change color"
-        color={`#${color}`}
-        onPress={() => {
-          setColor(randomColor());
-        }}
-      ></Button>
+    <View style={[styles.container]}>
+      {todos.map((todo) => (
+        <Todo todo={todo} key={todo}></Todo>
+      ))}
     </View>
   );
 }
